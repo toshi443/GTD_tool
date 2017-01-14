35925579ad71487ee47de0253fc192b5f7a54f97
獅々見俊明 <toshi443@mitoshiakinoMacBook-Pro.local>
2017-01-14 10:24:39 +0900

    update security

--- app/imports/api/tasks.js
+++ app/imports/api/tasks.js
@@ -4,6 +4,12 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer){
+  Meteor.publish('tasks', function tasksPublication(){
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
--- app/imports/ui/task.js
+++ app/imports/ui/task.js
@@ -3,6 +3,11 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 import './task.html';
 
+Template.task.helpers({
+  isOwner(){
+    return this.owner === Meteor.userId();
+  },
+});
 Template.task.events({
   'click .toggle-checked'(){
     Meteor.call('tasks.setChecked', this._id, !this.checked);
